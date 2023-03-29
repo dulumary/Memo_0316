67a4ea5022920b4118a4aa5253d1edb0d6ca3160
dulumary <lecture@hagulu.com>
2023-03-29 18:03:23 +0900

    [김인규]  - 게시물 수정 삭제, 파일 삭제 기능 추가

--- src/main/java/com/marondal/memo/common/FileManagerService.java
+++ src/main/java/com/marondal/memo/common/FileManagerService.java
@@ -6,11 +6,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileManagerService {
 	
 	public static final String FILE_UPLOAD_PATH = "D:\\김인규쌤\\web_1116\\springProject\\upload\\memo\\image"; 
+	
+	private static Logger logger = LoggerFactory.getLogger(FileManagerService.class);
 	
 	//파일 저장 -> 경로 생성
 	
@@ -32,6 +36,8 @@ public class FileManagerService {
 		File directory = new File(directoryPath);
 		if(!directory.mkdir()) {
 			// 디렉토리 생성 실패
+			
+			logger.error("saveFile : 디렉토리 생성 실패 " + directoryPath);
 			return null;
 		}
 		
@@ -47,6 +53,7 @@ public class FileManagerService {
 			
 		} catch (IOException e) {
 			
+			logger.error("saveFile : 파일 저장 실패 " + directoryPath);
 			e.printStackTrace();
 			
 			return null;
@@ -57,6 +64,52 @@ public class FileManagerService {
 		// http://localhost:8080/images/2_39i980139/test.png
 		
 		return "/images" + directoryName + file.getOriginalFilename();
+	}
+	
+	
+	// 파일 삭제 기능
+	public static boolean removeFile(String filePath) { // /images/2_1679477699709/blackbird-g9b42bb68b_640.jpg
+		
+		if(filePath == null) {
+			logger.info("삭제 대상 파일 없음");
+			return false;
+		}
+		// 실제 파일 저장 경로 찾기 
+		// /images 를 제거하고, 나머지 부분을 FILE_UPLOAD_PATH에 이어 붙인다. 
+		// D:\\김인규쌤\\web_1116\\springProject\\upload\\memo\\image/2_1679477699709/blackbird-g9b42bb68b_640.jpg
+		
+		String fullFilePath = FILE_UPLOAD_PATH + filePath.replace("/images", "");
+		Path path = Paths.get(fullFilePath);
+		
+		// 파일이 존재하는지 
+		if(Files.exists(path)) {
+			
+			try {
+				Files.delete(path);
+			} catch (IOException e) {
+				
+				logger.error("removeFile : 파일 삭제 에러 " + fullFilePath);
+				e.printStackTrace();
+				return false;
+			}
+		}
+		
+		// 디렉토리 제거
+		Path dirPath = path.getParent();
+		
+		if(Files.exists(dirPath)) {
+			try {
+				Files.delete(dirPath);
+			} catch (IOException e) {
+				logger.error("removeFile : 디렉토리 삭제 에러 " + fullFilePath);
+				e.printStackTrace();
+				return false;
+			}
+			
+		}
+		
+		return true;
+		
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/post/detail.jsp
+++ src/main/webapp/WEB-INF/jsp/post/detail.jsp
@@ -32,9 +32,9 @@
 				<div class="d-flex justify-content-between mt-3">
 					<div>
 						<a href="/post/list/view" class="btn btn-info"> 목록으로 </a>
-						<button type="button" class="btn btn-danger">삭제</button>
+						<button type="button" class="btn btn-danger" data-post-id="${post.id }" id="deleteBtn">삭제</button>
 					</div>
-					<button type="button" class="btn btn-primary" id="modifyBtn">수정</button>
+					<button type="button" data-post-id="${post.id }" class="btn btn-primary" id="modifyBtn">수정</button>
 				</div>
 			</div>
 		</section>
@@ -45,6 +45,65 @@
 	
 	<script>
 		$(document).ready(function() {
+			
+			$("#deleteBtn").on("click", function() {
+				
+				let postId = $(this).data("post-id");
+				
+				$.ajax({
+					type:"get"
+					, url:"/post/delete"
+					, data:{"postId":postId}
+					, success:function(data) {
+						if(data.result == "success") {
+							location.href = "/post/list/view";
+						} else {
+							alert("삭제 실패");
+						}
+						
+					}
+					, error:function() {
+						alert("삭제 에러");
+					}
+				});
+				
+			});
+			
+			$("#modifyBtn").on("click", function() {
+				
+				let postId = $(this).data("post-id");
+				
+				let title = $("#titleInput").val();
+				let content = $("#contentInput").val();
+				
+				if(title == "") {
+					alert("제목을 입력하세요");
+					return ;
+				}
+				
+				if(content == "") {
+					alert("내용을 입력하세요");
+					return ;
+				}
+				
+				$.ajax({
+					type:"post"
+					, url:"/post/update"
+					, data:{"postId":postId, "title":title, "content":content}
+					, success:function(data) {
+						
+						if(data.result == "success") {
+							location.reload();
+						} else {
+							alert("수정 실패");
+						}
+					}
+					, error:function() {
+						alert("수정 에러");
+					}
+					
+				});
+			});
 			
 		});
 	
